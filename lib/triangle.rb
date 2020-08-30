feb58c8711b85c4bc10aed6a157a92ca4fee88c3
Kelin Field <kfield111@hotmail.com>
2020-08-30 23:52:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,9 +6,9 @@ class Triangle
     @@all_sides = []
 
   def initialize(side_one, side_two, side_three)
-    @side_one = side_one
-    @side_two = side_two
-    @side_three = side_three
+    @side_one = side_one << @@all_sides
+    @side_two = side_two << @@all_sides
+    @side_three = side_three << @@all_sides
   end
 
 # binding.pry
