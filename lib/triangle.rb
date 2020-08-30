1a4ec0fa8309c73ca509b8b06d45430bf4f0ade9
Kelin Field <kfield111@hotmail.com>
2020-08-30 23:44:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Triangle
     attr_accessor :side_one, :side_two, :side_three
 
-  def initialize(side_one:, side_two:, side_three:)
+  def initialize(side_one, side_two, side_three)
     @side_one = side_one
     @side_two = side_two
     @side_three = side_three
