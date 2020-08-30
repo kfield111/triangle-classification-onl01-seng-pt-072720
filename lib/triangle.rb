4271d3d42a72fc2d3e40f088de7097ad2e034552
Kelin Field <kfield111@hotmail.com>
2020-08-30 23:59:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,21 +3,17 @@ require 'pry'
 class Triangle
     attr_accessor :side_one, :side_two, :side_three
 
-    @@all_sides = []
 
   def initialize(side_one, side_two, side_three)
     @side_one = side_one
     @side_two = side_two
     @side_three = side_three
-    @@all_sides << side_one
-    @@all_sides << side_two
-    @@all_sides << side_three
   end
 
 # binding.pry
 
   def kind
-    if @@all_sides.uniq.size < 1
+    if side_one == side_two && side_three
       :equilateral
     end
   end
