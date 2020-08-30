24b5cb7909f6df1db7952dcc6f3d8a725fb41e4b
Kelin Field <kfield111@hotmail.com>
2020-08-30 23:49:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
   def kind
     if side_one == side_two && side_three
       :equilateral
-    elsif side_one == side_two || side_three
+    elsif side_one == side_two || side_three != side_one
       :isosceles
     elsif side_one != side_two || side_three
       :scalene
