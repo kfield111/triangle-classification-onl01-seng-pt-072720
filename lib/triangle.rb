fc6331d0c27f1d9bc8cdd8503dd1a51e7c5d23c5
Kelin Field <kfield111@hotmail.com>
2020-08-31 00:31:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,13 +13,13 @@ class Triangle
 # binding.pry
 
   def kind
-    if (side_one == side_two) && (side_one == side_three) && (side_two == side_three)
+    if ((side_one == side_two) && (side_one == side_three) && (side_two == side_three))
       :equilateral
-    elsif (side_one == side_two) && ((side_one || side_two != side_three)
+    elsif ((side_one == side_two) && ((side_one || side_two != side_three))
       :isosceles
-    elsif (side_one == side_three) && ((side_one || side_three != side_two)
+    elsif ((side_one == side_three) && ((side_one || side_three != side_two))
       :isosceles
-    elsif (side_two == side_three) && ((side_two || side_three != side_one)
+    elsif ((side_two == side_three) && ((side_two || side_three != side_one))
       :isosceles
     else
       :scalene
