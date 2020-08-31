952f1b6f6807fe8a360f96de53c3d77a4a405169
Kelin Field <kfield111@hotmail.com>
2020-08-31 01:15:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ class Triangle
       :isosceles
     elsif (side_one <= 0 || side_two <= 0 || side_three <= 0)
       raise TriangleError
-    elsif (side_one + side_two < side_three ||  ||
+    elsif (side_one + side_two < side_three
       raise TriangleError
     elsif side_two + side_three < side_one
       raise TriangleError
