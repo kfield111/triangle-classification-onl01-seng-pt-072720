0a1e5d07f9ef3b8f00a03994a8dd2b767b8a93e8
Kelin Field <kfield111@hotmail.com>
2020-08-31 00:35:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,9 +18,17 @@ class Triangle
     elsif (side_one == side_two) || (side_two == side_three) || (side_one == side_three)
       :isosceles
     elsif side_one < 1 || side_two < 0 || side_three < 0
+      
     else
       :scalene
     end
+  end
+
+
+  def TraingleError
+
+
+    
   end
 
 end
