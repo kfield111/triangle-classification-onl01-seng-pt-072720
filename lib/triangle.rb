7ae7a0705fbb82c069fa94439f0bdd951a99b937
Kelin Field <kfield111@hotmail.com>
2020-08-31 00:32:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,12 +17,6 @@ class Triangle
       :equilateral
     elsif ((side_one == side_two) && ((side_one || side_two != side_three))
       :isosceles
-    elsif ((side_one == side_three) && ((side_one || side_three != side_two))
-      :isosceles
-    elsif ((side_two == side_three) && ((side_two || side_three != side_one))
-      :isosceles
-    else
-      :scalene
     end
   end
 
